c3f8a001e4949cee0b6f5161726517822040b8a7
UnAnsav <74003441+UnAnsav@users.noreply.github.com>
2023-05-14 22:10:02 +0300

    Update code_aiogram.py

--- code_aiogram.py
+++ code_aiogram.py
@@ -1,11 +1,12 @@
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import adsystemaiogram as adsystem
+import adsystemaiogram as adsystem      # import official library
 
 storage = MemoryStorage()       # Temporary data storage
-bot = Bot(TOKEN)
+bot = Bot(TOKEN)                # Bot
 dp = Dispatcher(bot, storage=storage)
 
 i_am_ = "@yourusername"     # YOUR username 
+# New handlers for correct operation
 dp.register_message_handler(adsystem.AdSystemConnectBot, adsystem.IAmTheOwner(i_am_))
 dp.register_message_handler(adsystem.AdSystemMessage, adsystem.MessageToAdSystem(bot))
 dp.register_callback_query_handler(adsystem.AdSystemCallback, adsystem.CallbackToAdSystem(bot), state="*")
