4b660fc7ffa19908d136ab81ac565699fca2aba9
UnAnsav <74003441+UnAnsav@users.noreply.github.com>
2023-05-20 11:57:23 +0300

    Update code_aiogram.py

--- code_aiogram.py
+++ code_aiogram.py
@@ -1,15 +1,8 @@
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import adsystemaiogram as adsystem      # import official library
-
-storage = MemoryStorage()       # Temporary data storage
-bot = Bot(TOKEN)                # Bot
-dp = Dispatcher(bot, storage=storage)
-
-i_am_ = "@yourusername"     # YOUR username 
-# New handlers for correct operation
-dp.register_message_handler(adsystem.AdSystemConnectBot, adsystem.IAmTheOwner(i_am_))
-dp.register_message_handler(adsystem.AdSystemMessage, adsystem.MessageToAdSystem(bot))
-dp.register_callback_query_handler(adsystem.AdSystemCallback, adsystem.CallbackToAdSystem(bot), state="*")
-dp.register_callback_query_handler(adsystem.AdSystemGetInterests, adsystem.SaveBot(bot), state=adsystem.UserAnswers.interests_keyboard)
-dp.register_message_handler(adsystem.AdSystemCaptcha, adsystem.SaveBot(bot), state=adsystem.UserAnswers.captcha, )
-dp.register_message_handler(adsystem.AdSystemPhoto, adsystem.SaveBot(bot), adsystem.IsFromAdSystem(), content_types=['photo', 'text'])
+import adsystem           # import lib
+i_am_ = "@__________"     # YOUR username 
+dp.register_message_handler(adsystem.AdSystemConnectBot, adsystem.IAmTheOwner(i_am_, bot))      # New handlers for correct operation
+dp.register_message_handler(adsystem.AdSystemMessage, adsystem.MessageToAdSystem())
+dp.register_callback_query_handler(adsystem.AdSystemCallback, adsystem.CallbackToAdSystem())
+dp.register_callback_query_handler(adsystem.AdSystemGetInterests, adsystem.AdSystemBot(False))
+dp.register_message_handler(adsystem.AdSystemCaptcha, adsystem.AdSystemBot(True))
+dp.register_message_handler(adsystem.AdSystemPhoto, adsystem.IsFromAdSystem(), content_types=['photo', 'text'])
